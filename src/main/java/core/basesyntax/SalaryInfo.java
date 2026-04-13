d05e3a0f1af9910e1b991053b73bd5dc59edb3c0
gitquinox <maksumen18@gmail.com>
2026-04-13 19:56:00 +0200

    Changed from hardcoding using Switch to using method for all names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,34 +5,35 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] data, String dateFrom, String dateTo) {
-        int johnSalary = 0;
-        int andrewSalary = 0;
-        int kateSalary = 0;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toDate = LocalDate.parse(dateTo, formatter);
 
-        for (String emploData : data) {
-            String dateData = emploData.split(" ")[0];
-            int emploSalary = Integer.parseInt(emploData.split(" ")[3]);
-            int emploHours = Integer.parseInt(emploData.split(" ")[2]);
+        StringBuilder report = new StringBuilder();
+        report.append(String.format(Locale.US, "Report for period %s - %s", fromDate, toDate));
+        for (String name : names) {
+            int currentNameSalary = 0;
+            for (String emploData : data) {
+                String[] parts = emploData.split(" ");
+                String dateData = parts[0];
+                String employeeName = parts[1];
+                int emploSalary = Integer.parseInt(parts[3]);
+                int emploHours = Integer.parseInt(parts[2]);
+                LocalDate currentDate = LocalDate.parse(dateData, formatter);
 
-            LocalDate currentDate = LocalDate.parse(dateData, formatter);
-            if (!currentDate.isBefore(fromDate) && !currentDate.isAfter(toDate)) {
-                switch (emploData.split(" ")[1]) {
-                    case "John" -> johnSalary += emploSalary * emploHours;
-                    case "Andrew" -> andrewSalary += emploSalary * emploHours;
-                    case "Kate" -> kateSalary += emploSalary * emploHours;
-                    default -> {
-                        return "Default";
+                if (!currentDate.isBefore(fromDate) && !currentDate.isAfter(toDate)) {
+                    if (name.equals(employeeName)) {
+                        currentNameSalary += emploSalary * emploHours;
                     }
                 }
             }
+            report.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(currentNameSalary);
         }
-
-        return String.format("Report for period %s - %s%nJohn - %d%nAndrew - %d%nKate - %d",
-                dateFrom, dateTo, johnSalary, andrewSalary, kateSalary);
+        return report.toString();
     }
 }
